1c450d11cbc934d92605a7a5ad030d5950d37b01
kaidong pei <>
2018-10-20 10:55:34 -0700

    cat wallet 2nd first commit

---
+++ cat-wallet-ios-2nd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cat-wallet-ios-2nd
+//
+//  Created by kaidong pei on 10/20/18.
+//  Copyright © 2018 CatWallet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
