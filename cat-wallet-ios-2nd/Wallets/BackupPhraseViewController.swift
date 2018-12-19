869a768c22177fb335e9c08ce6002c1a8c1cee30
kaidong pei <>
2018-12-18 18:11:05 -0800

    Add copy button in phrase viewcontroller

--- cat-wallet-ios-2nd/Wallets/BackupPhraseViewController.swift
+++ cat-wallet-ios-2nd/Wallets/BackupPhraseViewController.swift
@@ -12,9 +12,41 @@ class BackupPhraseViewController: UIViewController {
 
     @IBOutlet weak var phraseTextView: UITextView!
     var getText = ""
+    var copyButton: UIButton!
+    var buttonConstraint: NSLayoutConstraint!
+    let shownotibar = ShowNotiBar()
     override func viewDidLoad() {
         super.viewDidLoad()
         phraseTextView.text = getText
+        setCopyButton()
         // Do any additional setup after loading the view.
+    }
+    
+    func setCopyButton() {
+        copyButton = UIButton(type: .custom)
+        copyButton.backgroundColor = .black
+        copyButton.setTitle("Copy", for: .normal)
+        copyButton.tintColor = .white
+        self.view.addSubview(copyButton)
+        copyButton.translatesAutoresizingMaskIntoConstraints = false
+        copyButton.addTarget(self, action: #selector(copyAction), for: .touchUpInside)
+        copyButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        copyButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        buttonConstraint = copyButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -25)
+        buttonConstraint.isActive = true
+        copyButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        self.view.layoutIfNeeded()
+        copyButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        copyButton.layer.masksToBounds = false
+        copyButton.layer.cornerRadius = copyButton.frame.width / CGFloat(28)
+        copyButton.layer.borderWidth = 3.5
+    }
+    
+    @objc func copyAction() {
+        let pasteboard = UIPasteboard.general
+        pasteboard.string = phraseTextView.text
+        if pasteboard.string != nil {
+            shownotibar.showBar(title: "Public key is copied", subtitle: "", style: .success)
+        }
     }
 }
