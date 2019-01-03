eaf656654141ebf9bd74b8bc155a51aecde4c30f
kaidong pei <>
2019-01-03 11:25:03 -0800

    Add bitcoin payment

--- cat-wallet-ios-2nd/Bitcoin/BitcoinService.swift
+++ cat-wallet-ios-2nd/Bitcoin/BitcoinService.swift
@@ -12,6 +12,9 @@ import BitcoinKit
 var cryptoName = ""
 
 struct BitcoinService {
+    var peerGroup: PeerGroup?
+    var payments = [Payment]()
+    
     func createBTCWallet(_ mnemonic: String) -> HDWallet{
         let mnemonicList = mnemonic.components(separatedBy: " ")
             let seed = Mnemonic.seed(mnemonic: mnemonicList)
@@ -28,5 +31,57 @@ struct BitcoinService {
             print(error.localizedDescription)
         }
         return btcAddress
+    }
+    
+    func selectTx(from utxos: [UnspentTransaction], amount: Int64) -> (utxos: [UnspentTransaction], fee: Int64) {
+        return (utxos, 500)
+    }
+    
+    public func createUnsignedTx(toAddress: Address, amount: Int64, changeAddress: Address, utxos: [UnspentTransaction]) -> UnsignedTransaction {
+        let (utxos, fee) = selectTx(from: utxos, amount: amount)
+        let totalAmount: Int64 = utxos.reduce(0) { $0 + $1.output.value }
+        let change: Int64 = totalAmount - amount - fee
+        
+        let toPubKeyHash: Data = toAddress.data
+        let changePubkeyHash: Data = changeAddress.data
+        
+        let lockingScriptTo = Script.buildPublicKeyHashOut(pubKeyHash: toPubKeyHash)
+        let lockingScriptChange = Script.buildPublicKeyHashOut(pubKeyHash: changePubkeyHash)
+        
+        let toOutput = TransactionOutput(value: amount, lockingScript: lockingScriptTo)
+        let changeOutput = TransactionOutput(value: change, lockingScript: lockingScriptChange)
+        
+        let unsignedInputs = utxos.map { TransactionInput(previousOutput: $0.outpoint, signatureScript: Data(), sequence: UInt32.max) }
+        let tx = Transaction(version: 1, inputs: unsignedInputs, outputs: [toOutput, changeOutput], lockTime: 0)
+        return UnsignedTransaction(tx: tx, utxos: utxos)
+    }
+    
+    func signTx(unsignedTx: UnsignedTransaction, keys: [PrivateKey]) -> Transaction {
+        var inputsToSign = unsignedTx.tx.inputs
+        var transactionToSign: Transaction {
+            return Transaction(version: unsignedTx.tx.version, inputs: inputsToSign, outputs: unsignedTx.tx.outputs, lockTime: unsignedTx.tx.lockTime)
+        }
+        
+        let hashType = SighashType.BTC.ALL
+        for (i, utxo) in unsignedTx.utxos.enumerated() {
+            let pubkeyHash: Data = Script.getPublicKeyHash(from: utxo.output.lockingScript)
+            
+            let keysOfUtxo: [PrivateKey] = keys.filter { $0.publicKey().pubkeyHash == pubkeyHash }
+            guard let key = keysOfUtxo.first else {
+                print("No keys to this txout : \(utxo.output.value)")
+                continue
+            }
+            print("Value of signing txout : \(utxo.output.value)")
+            
+            let sighash: Data = transactionToSign.signatureHash(for: utxo.output, inputIndex: i, hashType: SighashType.BTC.ALL)
+            let signature: Data = try! Crypto.sign(sighash, privateKey: key)
+            let txin = inputsToSign[i]
+            let pubkey = key.publicKey()
+            
+            let unlockingScript = Script.buildPublicKeyUnlockingScript(signature: signature, pubkey: pubkey, hashType: hashType)
+        
+            inputsToSign[i] = TransactionInput(previousOutput: txin.previousOutput, signatureScript: unlockingScript, sequence: txin.sequence)
+        }
+        return transactionToSign
     }
 }
